7544aadcdb3de861bba45e732eef3f7a97f5f1af
ASplayer9119 <pokeras911@mail.ru>
2019-03-20 16:37:53 +0300

    Ex 2.75 - make-from-mag-ang in message passing style

---
+++ solutions/chapter2/75.rkt
@@ -0,0 +1,10 @@
+#lang racket/base
+
+(define (make-from-mag-ang r a)
+  (define (dispatch op)
+    (cond ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)
+          ((eq? op 'real-part) (* r (cos a)))
+          ((eq? op 'imag-part) (* r (sin a)))
+          (else (error "Unknown op: MAKE-FROM-MAG-ANG" op))))
+  dispatch)
