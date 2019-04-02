048f11f53ac7f78f7e3b767d9480dfacb715e744
ASplayer9119 <pokeras911@mail.ru>
2019-04-02 12:28:28 +0300

    Ex 3.18 - has-cycle?

---
+++ solutions/chapter3/18.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+(define (last-pair x)
+  (if (null? (cdr x)) x (last-pair (cdr x))))
+
+(define (has-cycle? x)
+  (define visited '())
+  (define (visited? x)
+    (if (memq x visited)
+        #t
+        #f))
+  (define (traverse x)
+    (cond ((null? x) #f)
+          ((visited? x) #t)
+          (else (begin (set! visited (cons x visited))
+                       (traverse (cdr x))))))
+  (traverse x))
+
+(define l (list 1 2 3))
+(has-cycle? l)
+
+(append! l l)
+(has-cycle? l)
