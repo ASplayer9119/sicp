780aceaf7107c27091d734fddc889ff01ad8d92e
ASplayer9119 <pokeras911@mail.ru>
2019-04-03 11:32:30 +0300

    Ex 3.21 - print-queue

---
+++ solutions/chapter3/21.rkt
@@ -0,0 +1,52 @@
+#lang sicp
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else (set-cdr! (rear-ptr queue) new-pair)
+                (set-rear-ptr! queue new-pair)
+                queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else (set-front-ptr! queue (cdr (front-ptr queue)))
+              queue)))
+
+(define (print-queue queue)
+  (define (iter x)
+    (cond ((null? x) (display "]") (newline))
+          (else (display (car x))
+                (if (null? (cdr x))
+                    (display "")
+                    (display ","))
+                (iter (cdr x)))))
+  (display "[")
+  (iter (front-ptr queue)))
+
+(define q1 (make-queue))
+(print-queue q1)
+(print-queue (insert-queue! q1 'a))
+(print-queue (insert-queue! q1 'b))
+(print-queue (delete-queue! q1))
+(print-queue (delete-queue! q1))
