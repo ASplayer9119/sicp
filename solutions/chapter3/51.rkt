b73f3ded1890236c553109d456232a820ce147db
ASplayer9119 <pokeras911@mail.ru>
2019-04-11 12:58:05 +0300

    Ex 3.51 - stream-map with show

---
+++ solutions/chapter3/51.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+
+(#%require "helpers.rkt")
+
+(define (show x)
+  (display-line x)
+  x)
+
+(define x
+  (stream-map show (stream-enumerate-interval 0 10)))
+(stream-ref x 5)
+(stream-ref x 7)
