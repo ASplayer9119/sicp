4e6e96541fadbab9d6bc49dd1beba483d3d0b7a5
ASplayer9119 <pokeras911@mail.ru>
2019-04-11 13:09:03 +0300

    Ex 3.52 - stream with sum

---
+++ solutions/chapter3/52.rkt
@@ -0,0 +1,15 @@
+#lang sicp
+
+(#%require "helpers.rkt")
+
+(define sum 0)
+(define (accum x) (set! sum (+ x sum)) sum)
+(define seq
+  (stream-map accum
+              (stream-enumerate-interval 1 20)))
+(define y (stream-filter even? seq))
+(define z
+  (stream-filter (lambda (x) (= (remainder x 5) 0))
+                 seq))
+(stream-ref y 7)
+(display-stream z)
